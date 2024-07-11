3de70235cdc86df769a1bd30347917772b33cb99
Antonia Tan <antonitan@meta.com>
2024-07-11 11:06:06 +0800

    updated eslintrc to new eslint config

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+"use strict";
+
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  ...compat.extends("plugin:@typescript-eslint/recommended").map((config) => ({
+    ...config,
+    files: ["**/src/*.js", "**/src/*.ts", "**/src/*.tsx"],
+  })),
+  ...compat.extends("plugin:react-hooks/recommended").map((config) => ({
+    ...config,
+    files: ["**/src/*.js", "**/src/*.ts", "**/src/*.tsx"],
+  })),
+  ...compat.extends("fbjs").map((config) => ({
+    ...config,
+    files: ["**/src/*.js", "**/src/*.ts", "**/src/*.tsx"],
+  })),
+  {
+    ...js.configs.recommended,
+    languageOptions: {
+      sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: "latest",
+    },
+    plugins: { reactRefresh },
+    rules: {
+      "reactRefresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "prefer-const": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    },
+  },
+];
---
+++ src/test.js
@@ -0,0 +1,4 @@
+export const hello = () => {
+  const a = "hello";
+  return a;
+};
